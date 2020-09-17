8b5ca5adc2591ede0097fd0d79ff051100adad6e
wenshunpeng <wenshunpeng@xunxintech.cn>
2020-09-17 15:34:22 +0800

    更新MOD信息 创建Music box物品

--- src/main/java/team/info/ncmfm/NcmMod.java
+++ src/main/java/team/info/ncmfm/NcmMod.java
@@ -1,17 +1,16 @@
-package com.example.examplemod;
+package team.info.ncmfm;
 
 import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@net.minecraftforge.fml.common.Mod(modid = NcmMod.MODID, name = NcmMod.NAME, version = NcmMod.VERSION)
+public class NcmMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "ncmfm";
+    public static final String NAME = "NeteaseCloudMusicMod";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -25,7 +24,8 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
+
+
 }
---
+++ src/main/java/team/info/ncmfm/item/ItemMusicBox.java
@@ -0,0 +1,10 @@
+package team.info.ncmfm.item;
+
+import net.minecraft.item.Item;
+
+public class ItemMusicBox extends Item {
+    public ItemMusicBox(){
+        this.setRegistryName("music_box");
+        this.setUnlocalizedName("musicBox");
+    }
+}
