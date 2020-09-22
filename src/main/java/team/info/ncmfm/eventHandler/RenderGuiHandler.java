24eb31b6b5cfe699093db99d751ccc4670e00c06
foxcell <396191931@qq.com>
2020-09-22 10:58:35 +0800

    加入代理类

--- src/main/java/team/info/ncmfm/eventHandler/RenderGuiHandler.java
+++ src/main/java/team/info/ncmfm/eventHandler/RenderGuiHandler.java
@@ -1,15 +1,14 @@
-package team.info.ncmfm;
+package team.info.ncmfm.eventHandler;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import team.info.ncmfm.ui.Pannel;
 
-@Mod.EventBusSubscriber
 public class RenderGuiHandler {
+
     @SubscribeEvent
-    public static void onRenderGui(RenderGameOverlayEvent.Post event)
+    public void onRenderGui(RenderGameOverlayEvent.Post event)
     {
         if (event.getType() != RenderGameOverlayEvent.ElementType.EXPERIENCE) {
             return;
