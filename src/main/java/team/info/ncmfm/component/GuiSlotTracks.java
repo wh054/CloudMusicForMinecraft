39da120156c5193e6b42abd519d62ed9044ca841
foxcell <396191931@qq.com>
2020-09-25 00:18:47 +0800

    歌曲列表选取完善

--- src/main/java/team/info/ncmfm/component/GuiSlotTracks.java
+++ src/main/java/team/info/ncmfm/component/GuiSlotTracks.java
@@ -6,6 +6,7 @@ import net.minecraft.util.StringUtils;
 import net.minecraftforge.fml.client.GuiModList;
 import net.minecraftforge.fml.client.GuiScrollingList;
 import net.minecraftforge.fml.common.ModContainer;
+import team.info.ncmfm.model.PlayListContainer;
 import team.info.ncmfm.model.TrackContainer;
 import team.info.ncmfm.ui.MusicPannel;
 
@@ -14,39 +15,52 @@ import java.util.ArrayList;
 public class GuiSlotTracks extends GuiScrollingList {
 
     private MusicPannel parent;
+    private ArrayList<TrackContainer> collections;
+    private int slotHeight;
 
     public GuiSlotTracks(MusicPannel parent, ArrayList<TrackContainer> trackList, int listWidth, int slotHeight)
     {
-        super(parent.getMinecraftInstance(), listWidth, parent.height, 32, parent.height - 88 + 4, 10, slotHeight, parent.width, parent.height);
+        super(parent.getMinecraftInstance(), parent.width/2-30, parent.height, 32, parent.height - 88 + 4, parent.width/2+20, slotHeight, parent.width, parent.height);
         this.parent = parent;
+        this.collections=trackList;
+        this.slotHeight=slotHeight;
     }
 
     @Override
     protected int getSize() {
-        return 0;
+        return this.collections.size();
     }
 
     @Override
     protected void elementClicked(int index, boolean doubleClick) {
-
+            this.parent.selectTrackIndex(index);
+            if(doubleClick){
+                //play music
+                this.parent.PlayMusic();
+            }
     }
 
     @Override
     protected boolean isSelected(int index) {
-        return false;
+        return this.parent.trackIndexSelected(index);
     }
 
     @Override
     protected void drawBackground() {
-        this.parent.drawDefaultBackground();
+
+    }
+
+    @Override
+    protected int getContentHeight() {
+        return (this.getSize()) * slotHeight + 1;
     }
 
     @Override
     protected void drawSlot(int slotIdx, int entryRight, int slotTop, int slotBuffer, Tessellator tess) {
-        TrackContainer tc     = null;
+        TrackContainer tc     = this.collections.get(slotIdx);
         String       name     = StringUtils.stripControlCodes(tc.getName());
         FontRenderer font     = this.parent.getFontRenderer();
 
-        font.drawString(font.trimStringToWidth(name,    listWidth - 10), this.left + 3 , top +  2, 0xFFFFFF);
+        font.drawString(font.trimStringToWidth(name,listWidth - 10), this.left + 3 , slotTop +  2, 0xFFFFFF);
     }
 }
--- src/main/java/team/info/ncmfm/ui/MusicPannel.java
+++ src/main/java/team/info/ncmfm/ui/MusicPannel.java
@@ -26,7 +26,7 @@ public class MusicPannel extends GuiScreen {
     public final int height;
 
     private int playListWidth;
-    private int tracksWidth;
+    private int tracksWidth=200;
 
     private static final int BUTTON_GET_MUSICLIST = 0;
 
@@ -36,8 +36,10 @@ public class MusicPannel extends GuiScreen {
     private GuiSlotPlayList slotPlayList;
     private GuiSlotTracks slotTracks;
 
-    private int selected = -1;
+    private int playList_selected_index = -1;
+    private int track_selected_index=-1;
     private PlayListContainer selectedPlayList;
+    private TrackContainer selectedTrack;
 
     private IMusicManager musicManager;
 
@@ -52,15 +54,21 @@ public class MusicPannel extends GuiScreen {
 
     @Override
     public void initGui() {
-        int slotHeight = 25;
+        int slotHeight = 15;
         playList.addAll(musicManager.LoadPlayList());
         for (PlayListContainer plc : playList)
         {
             playListWidth = Math.max(playListWidth,getFontRenderer().getStringWidth(plc.getName()) + 10);
         }
 
+        for (TrackContainer tc : trackList)
+        {
+            tracksWidth = Math.max(tracksWidth,getFontRenderer().getStringWidth(tc.getName()) + 10);
+        }
+
         this.buttonList.add(new GuiButton(BUTTON_GET_MUSICLIST,(int)(width*0.75), (int)(height*0.85), 80, 20,"更新歌单"));
         this.slotPlayList = new GuiSlotPlayList(this, playList, playListWidth, slotHeight);
+        this.slotTracks=new GuiSlotTracks(this,trackList,tracksWidth,slotHeight);
     }
 
     @Override
@@ -68,6 +76,7 @@ public class MusicPannel extends GuiScreen {
         mc.renderEngine.bindTexture(texture);
         drawScaledCustomSizeModalRect(0, 0, 0, 0, 1828, (int)(1828*(height/(float)width)), width, height, 1828, 1292);
         this.slotPlayList.drawScreen(mouseX, mouseY, partialTicks);
+        this.slotTracks.drawScreen(mouseX, mouseY, partialTicks);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -100,22 +109,39 @@ public class MusicPannel extends GuiScreen {
 
     public void selectPlayListIndex(int index)
     {
-        if (index == this.selected)
+        if (index == this.playList_selected_index)
             return;
-        this.selected = index;
-        this.selectedPlayList = (index >= 0 && index <= playList.size()) ? playList.get(selected) : null;
+        this.playList_selected_index = index;
+        this.selectedPlayList = (index >= 0 && index <= playList.size()) ? playList.get(playList_selected_index) : null;
     }
+
+    public void selectTrackIndex(int index){
+        if (index == this.track_selected_index){
+            return;
+        }
+        this.track_selected_index = index;
+        this.selectedTrack = (index >= 0 && index <= trackList.size()) ? trackList.get(track_selected_index) : null;
+    }
+
 
     public boolean playListIndexSelected(int index)
     {
-        return index == selected;
+        return index == playList_selected_index;
+    }
+
+    public boolean trackIndexSelected(int index){
+        return index==track_selected_index;
     }
 
     public void LoadTrackList(){
         trackList.clear();
-        PlayListContainer plc= playList.get(this.selected);
+        PlayListContainer plc= playList.get(this.playList_selected_index);
         if(plc!=null){
             trackList.addAll(musicManager.LoadTrackList(plc.getId()));
         }
+    }
+
+    public void PlayMusic(){
+        System.out.println("当前播放歌曲:"+"[ID]"+this.selectedTrack.getId()+"|"+this.selectedTrack.getName());
     }
 }
