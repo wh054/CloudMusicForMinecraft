c6ad2f1416f5020a42d72b26d0157367b4b14486
foxcell <396191931@qq.com>
2020-10-13 14:49:58 +0800

    手动释放流处理

--- src/main/java/team/info/ncmfm/audio/MediaPlayer.java
+++ src/main/java/team/info/ncmfm/audio/MediaPlayer.java
@@ -3,6 +3,9 @@ package team.info.ncmfm.audio;
 import javazoom.jl.decoder.Bitstream;
 import javazoom.jl.decoder.BitstreamException;
 import javazoom.jl.player.Player;
+import net.minecraft.util.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -11,6 +14,7 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public abstract class MediaPlayer {
+    private static final Logger logger = LogManager.getLogger(MediaPlayer.class);
     private String url="";
 
     private InputStream inputStream=null;
@@ -19,39 +23,42 @@ public abstract class MediaPlayer {
 
     public MediaPlayer(String url){
         this.url=url;
+        logger.info("Ã½ÌåÁ´½Ó ==>> "+url);
     }
 
     public void play() {
         try{
-            URL uurl = new URL(this.url);
-            URLConnection con = uurl.openConnection();
-            inputStream = con.getInputStream();
-            buffer = new BufferedInputStream(inputStream);
-            bt = new Bitstream(buffer);
-            Player player = new Player(buffer);
-            player.play();
+            if(!StringUtils.isNullOrEmpty(this.url)){
+                URL uurl = new URL(this.url);
+                URLConnection con = uurl.openConnection();
+                inputStream = con.getInputStream();
+                buffer = new BufferedInputStream(inputStream);
+                bt = new Bitstream(buffer);
+                Player player = new Player(buffer);
+                player.play();
+            }
         }catch (Exception ex){
-            System.out.println(ex.getMessage());
+            logger.error(ex.getMessage());
         }finally {
             if(inputStream!=null){
                 try{
                     inputStream.close();
                 }catch (IOException ex){
-
+                    logger.error(ex.getMessage());
                 }
             }
             if(buffer!=null){
                 try{
                     buffer.close();
                 }catch (IOException ex){
-
+                    logger.error(ex.getMessage());
                 }
             }
             if(bt!=null){
                 try{
                     bt.close();
                 }catch (BitstreamException ex){
-
+                    logger.error(ex.getMessage());
                 }
             }
         }
